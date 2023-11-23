1befe12e46c3ac0de976401b8c8323726ba7fbd0
Jeffry Dela Cruz <jeperydcruz@gmail.com>
2023-11-24 05:35:37 +0800

    test email

--- tu.js
+++ tu.js
@@ -5,3 +5,13 @@ let TitleUnderwriterNumber = localStorage.getItem('tu-number');
 
 document.getElementById('tu-agent').textContent = TitleUnderwriterAgentName;
 document.getElementById('tu-number').textContent = TitleUnderwriterNumber;
+
+// email
+
+TUEmail.onclick = function sendMail() {
+
+    var link = "mailto=" + document.getElementById('TUnderwriterEmail').value + "&subject=Hi" ;
+
+    window.location.href = link;
+
+};
